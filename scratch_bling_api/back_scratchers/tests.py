00d8f564ce613abc3f8a94e6d5dd5cad90dc0a81
joeblack24 <jvw8@georgetown.edu>
2021-05-20 18:54:52 -0400

    updated static files

--- scratch_bling_api/back_scratchers/tests.py
+++ scratch_bling_api/back_scratchers/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import BackScratchers, Size
 # Create your tests here.
+
+class BackScratcherTestCase(TestCase):
+    def test_back_scratcher(self):
+        self.back_scratcher = BackScratchers(name='Test Scratcher', description='Testing')
+        self.assertEqual(self.back_scratcher.name, 'Test Scratcher')
+        self.assertEqual(self.back_scratcher.description, 'Testing')
+
+    def test_size(self):
+        self.test_size = Size()
+        self.test_size.size = 'S'
+        self.assertEqual(self.test_size.size, 'S')

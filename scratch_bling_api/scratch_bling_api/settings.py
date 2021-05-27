008d011ba206005e751bc38048ff43f5e44c454e
joeblack24 <jvw8@georgetown.edu>
2021-05-27 14:36:30 -0400

    updated settings again

--- scratch_bling_api/scratch_bling_api/settings.py
+++ scratch_bling_api/scratch_bling_api/settings.py
@@ -94,15 +94,14 @@ WSGI_APPLICATION = 'scratch_bling_api.wsgi.application'
 #     }
 # }
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Testapp1',
-        'HOST': 'bytelion-app-db.cytsxk6o94j1.us-east-2.rds.amazonaws.com',
-        'PORT': 5432,
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
 }
 
